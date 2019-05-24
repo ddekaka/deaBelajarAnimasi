33228a22e5ee12398a01febe712b801e9f20c79e
Dea Karina Khaerunnisa <dea.karina.k@gmail.com>
2019-05-24 19:39:48 +0700

    Initial Commit

---
+++ deaBelajarAnimasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deaBelajarAnimasi
+//
+//  Created by Dea Khaerunnisa on 24/05/19.
+//  Copyright © 2019 dea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
