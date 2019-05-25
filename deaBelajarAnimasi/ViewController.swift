f4c0340a4ce73648a7dd61701995c3fcf1e35ac6
Dea Karina Khaerunnisa <dea.karina.k@gmail.com>
2019-05-25 15:26:24 +0700

    initial commit

--- deaBelajarAnimasi/ViewController.swift
+++ deaBelajarAnimasi/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let layerDea = CAGradientLayer()
+        layerDea.frame = CGRect(x: 64, y: 64, width: 160, height: 160)
+        layerDea.colors = [UIColor.red.cgColor, UIColor.black.cgColor]
+        view.layer.addSublayer(layerDea)
+        
+        let rotateAnimation = CABasicAnimation()
+        rotateAnimation.valueFunction = CAValueFunction(name: CAValueFunctionName.rotateZ)
+        rotateAnimation.fromValue = 0
+        rotateAnimation.toValue = Float.pi
+        rotateAnimation.duration = 3
+        layerDea.add(rotateAnimation, forKey: "transform")
     }
 
 
